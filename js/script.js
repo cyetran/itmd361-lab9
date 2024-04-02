4eaf78c2299d54b82dc4a44d988e1ea0eb8f78f8
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-02 12:26:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+ function myEventFunction() {
+  alert('Hello');
+ }
 
+  var d4 = document.getElementByID('div4');
+  d4.addEventListener('click', myEventFunction);
+  d4.addEventListener('click', function() {
+   console.log('I was Clicked!');
+   d4.style.backgroundColor = '#COO';
+   d4.innerHTML = '<strong>Goodbye</strong>';
+  });
+}
+ 
 
 
 
